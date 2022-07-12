a1670b8323338afa73baaa4a66d285cd9194b34b
whid-bb <whid11s50@gmail.com>
2022-07-12 10:14:37 +0200

    remove comments

--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
-// import {createStore } from 'redux'
 import App from './App';
 
-// const store = createStore()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
