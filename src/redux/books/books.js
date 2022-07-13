32b03a8110326e2a804a5cdbd9de0e04162e6d6b
whid-bb <whid11s50@gmail.com>
2022-07-13 17:00:40 +0200

    update action creators

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,52 +1,92 @@
+import { v4 as uuidv4 } from 'uuid';
+import BookService from '../../services/BooksService';
+
 const ADD = 'bookstore/books/ADD';
 const REMOVE = 'bookstore/books/REMOVE';
+const GET_ALL = 'bookstore/books/GET_ALL';
 
-const initState = [
-  {
-    id: 1,
-    title: 'TestTitle',
-    author: 'TestAuthor',
-  },
-  {
-    id: 2,
-    title: 'TestTitlePart2',
-    author: 'TestAuthorPart2',
-  },
-];
+const initState = {
+  1: [
+    {
+      title: 'TestTitle',
+      author: 'TestAuthor',
+    },
+  ],
+  2: [
+    {
+      title: 'TestTitlePart2',
+      author: 'TestAuthorPart2',
+    },
+  ],
+};
 
 const booksReducer = (state = initState, action) => {
-  let nextId = 0;
-  if ((state.length - 1) < 0) {
-    nextId = 0;
-  } else {
-    nextId = state[state.length - 1].id;
-  }
-  nextId += 1;
-
   switch (action.type) {
+    case GET_ALL:
+      return action.payload;
     case ADD:
-      return [...state, {
-        id: nextId,
-        title: action.title,
-        author: action.author,
-      }];
+      return { ...state, ...action.payload };
     case REMOVE:
-
-      return state.filter((book) => book.id !== action.id);
+      // eslint-disable-next-line max-len
+      return Object.fromEntries(Object.entries(state).filter((book) => book[0] !== action.payload.id));
     default:
       return state;
   }
 };
 
-export const addAction = (title, author) => ({
-  type: ADD,
-  title,
-  author,
-});
+export const getAllAction = () => async (dispatch) => {
+  try {
+    const res = await BookService.getAll();
+    dispatch({
+      type: GET_ALL,
+      payload: res.data,
+    });
+  } catch (err) {
+    throw new Error(err);
+  }
+};
 
-export const removeAction = (id) => ({
-  type: REMOVE,
-  id,
-});
+export const addAction = (title, author) => async (dispatch) => {
+  try {
+    // eslint-disable-next-line camelcase
+    const item_id = uuidv4();
+    const category = null;
+    const newBook = {
+      // eslint-disable-next-line camelcase
+      item_id,
+      title,
+      author,
+      category,
+    };
+    await BookService.add(newBook);
+    dispatch({
+      type: ADD,
+      payload: {
+        // eslint-disable-next-line camelcase
+        item_id: [
+          {
+            title,
+            author,
+            category,
+          },
+        ],
+      },
+    });
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+export const removeAction = (id) => async (dispatch) => {
+  try {
+    await BookService.remove(id);
+    dispatch({
+      type: REMOVE,
+      payload: { id },
+    });
+  } catch (err) {
+    throw new Error(err);
+  }
+};
 
 export default booksReducer;
