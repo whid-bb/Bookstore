b659f45ce1f7b88c817dfa0e6fd0aa2fdce4dd41
whid-bb <whid11s50@gmail.com>
2022-07-13 17:01:06 +0200

    update components to receive from API

--- src/components/books/Books.js
+++ src/components/books/Books.js
@@ -1,19 +1,26 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllAction } from '../../redux/books/books';
 import Book from './Book';
 import Addbook from './Addbook';
 
 function Books() {
   const books = useSelector((state) => state.books);
-
+  // console.log(books);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllAction());
+  }, []);
+  // console.log(books);
   return (
     <>
       <ul>
-        {books.map((book) => (
+        {Object.keys(books).map((book) => (
           <Book
-            key={book.id}
-            bookId={book.id}
-            title={book.title}
-            author={book.author}
+            key={book}
+            bookId={book}
+            title={books[book][0].title}
+            author={books[book][0].author}
           />
         ))}
       </ul>
